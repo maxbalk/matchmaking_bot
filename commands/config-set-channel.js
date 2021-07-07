80aef40b0a9a8df4a40cfbf263313afe57a1e6d1
JakeBw <bowden9692@gmail.com>
2021-07-07 08:35:07 -0400

    set guildID

--- commands/config-set-channel.js
+++ commands/config-set-channel.js
@@ -18,6 +18,7 @@ module.exports = {
         }
         const chan = channel_table.create({
             name: command,
+            guildID: guildID
         });
 
         var res = `Bot channel set to: ${command} in ${guildID}.`;
