156d51f7350eabb6756b12535dda50c89113e145
JakeBw <bowden9692@gmail.com>
2021-07-07 07:59:00 -0400

    removed unused var

--- commands/list-roles.js
+++ commands/list-roles.js
@@ -7,7 +7,6 @@ module.exports = {
         const roles_table = Models.roles();
         const currentID = message.guild.id;
         const modelList = await roles_table.findAll({ attributes: ['name', 'guild_id', 'active'] });
-        const modelString = modelList.map(t => t.name).join(', ') || 'No roles set.';
 
         const listOfRoles = [];
 
