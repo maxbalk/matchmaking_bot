91cccc1f45fd1ad386f29129a793732e0b160ff9
JakeBw <bowden9692@gmail.com>
2021-07-07 08:33:03 -0400

    whitespace

--- commands/config-set-channel.js
+++ commands/config-set-channel.js
@@ -4,24 +4,23 @@ module.exports = {
 	name: 'config-set-channel',
 	description: 'Sets a new channel for the guild.',
 	async execute(message, args) {
-		var guildID = message.guild.id.toString();
+        var guildID = message.guild.id.toString();
         const command = args.toString();
 
-		const channel_table = Models.channel();
-		const matchingChan = await channel_table.findOne({ where: { guild_id: guildID } });
+        const channel_table = Models.channel();
+        const matchingChan = await channel_table.findOne({ where: { guild_id: guildID } });
 
-		if(matchingChan != null) {
-			var res = `Updated channel to: ${command} `;
+        if(matchingChan != null) {
+            var res = `Updated channel to: ${command} `;
             const newRows = await channel_table.update({ name: command }, { where: { name: command, guild_id: guildID }});
-			message.channel.send(res);
-			return;
-		}
-		const chan = channel_table.create({
-			name: command,
-			guild_id: guildID
-		});
+            message.channel.send(res);
+            return;
+        }
+        const chan = channel_table.create({
+            name: command,
+        });
 
-		var res = `Bot channel set to: ${command} in ${guildID}.`;
-		message.channel.send(res);
-	}
+        var res = `Bot channel set to: ${command} in ${guildID}.`;
+        message.channel.send(res);
+    }
 };
