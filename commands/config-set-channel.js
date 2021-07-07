954716f0f0be078b806defa4b989568a5b6c9cc4
JakeBw <bowden9692@gmail.com>
2021-07-07 08:28:33 -0400

    config set channel command added, added model for saving channel data

---
+++ commands/config-set-channel.js
@@ -0,0 +1,27 @@
+const Models = require('../lib/models')
+
+module.exports = {
+	name: 'config-set-channel',
+	description: 'Sets a new channel for the guild.',
+	async execute(message, args) {
+		var guildID = message.guild.id.toString();
+        const command = args.toString();
+
+		const channel_table = Models.channel();
+		const matchingChan = await channel_table.findOne({ where: { guild_id: guildID } });
+
+		if(matchingChan != null) {
+			var res = `Updated channel to: ${command} `;
+            const newRows = await channel_table.update({ name: command }, { where: { name: command, guild_id: guildID }});
+			message.channel.send(res);
+			return;
+		}
+		const chan = channel_table.create({
+			name: command,
+			guild_id: guildID
+		});
+
+		var res = `Bot channel set to: ${command} in ${guildID}.`;
+		message.channel.send(res);
+	}
+};
