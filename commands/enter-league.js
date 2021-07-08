579adce4ae8dd799666f4b0095c67966109c1b33
max <maximumbalk@gmail.com>
2021-07-08 07:37:00 -0500

    user id for rater player instead of tag. can leave league

--- commands/enter-league.js
+++ commands/enter-league.js
@@ -11,38 +11,37 @@ module.exports = {
 		})
 		const leagueMemberRole = message.guild.roles.cache
 			.filter(role => role.id == myLeague.member_role_id).array()[0]
-		var role = message.member.guild.roles.cache.find(role => role.name === leagueMemberRole.name);
-
-		if(role == null)
-		{
-			var badRes = `Registered cannot be assigned in this server, check if Registered role exists, make sure bot role is above Registered role.`;
-			message.channel.send(badRes);
-			return;
-		}
+		var role = message.guild.roles.cache.find(role => role.name === leagueMemberRole.name);
 
 		const r_player_table = Models.rated_player();
-		const matchingRole = await r_player_table.findOne({ where: { name: message.member.user.tag, league_id: guildID } });
+		const matchingPlayer = await r_player_table.findOne({ 
+			where: { 
+				user_id: message.author.id, 
+				league_id: guildID
+			} 
+		});
 
-		if(matchingRole != null) {
-			var badRes = `${message.member.user.tag} is already registered.`;
+		if(matchingPlayer != null && matchingPlayer.active) {
+			var badRes = `${message.author.tag} is already registered.`;
 			message.channel.send(badRes);
 			return;
 		}
 
 		const setRole = await message.guild.members.cache.get(message.author.id).roles.add(role).catch(err => message.channel.send(err.toString()));
-
 		if(setRole.content != null)
 		{
 			if(setRole.content.includes('Missing Access')) return;	
 		}
-
-		const rated_player = r_player_table.create({
-			name: message.member.user.tag,
-			elo: 1000,
-			league_id: guildID
-		});
-
-		var res = `${message.member.user.tag} has successfully registered.`;
+		if(matchingPlayer == null){
+			const new_rated_player = r_player_table.create({
+				user_id: message.author.id,
+				elo: 1000,
+				league_id: guildID,
+				active: true
+			});
+		}
+	
+		var res = `${message.author.tag} has successfully entered the league.`;
 		message.channel.send(res);
 	}
 };
