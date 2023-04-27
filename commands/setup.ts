b61794515431ce53d78e83d751725a0bd2b205de
JakeBw <bowden9692@gmail.com>
2023-04-27 10:11:51 -0400

    fixed admin role setup

--- commands/setup.ts
+++ commands/setup.ts
@@ -17,16 +17,15 @@ export = {
   },
 
   async findMatchingRole(message: Message, roleName: String) {
-    const match = message.guild.roles.cache.values();
-    for (let i in match){
-      if (match[i] === roleName){
-        console.log(match[i].name)
-        return match[i].id
-      }
-      else{
-        return
+    const roles = message.guild.roles.cache.values();
+    for (const role of roles) {
+      if (role.name === roleName) {
+        console.log(role.name);
+        console.log(role.id);
+        return role;
       }
     }
+    return 'fail';
   },
   async findMatchingChannel(
     message: Message,
@@ -45,42 +44,48 @@ export = {
   },
 
   async adminSetup(message: Message, client: CommandClient) {
-    const result = await this.trueAdminSetup(message, client)
-    console.log(result)
+    const result = await this.trueAdminSetup(message, client);
+    console.log(result);
+    if(result === 'success') {
+      return
+    } else {
+      await this.adminSetup(message, client)
+    }
   },
 
   async trueAdminSetup(message: Message, client: CommandClient) {
     const guildID = message.guild.id;
-    let league = await findGuildLeague(message.guild.id)
-     message.channel.send("Please enter the name of the admin role");
+    let league = await findGuildLeague(message.guild.id);
+    message.channel.send("Please enter the name of the admin role");
     try {
       const adminRoleCollection = await this.collectResponse(message);
       const match = await this.findMatchingRole(
         message,
         adminRoleCollection.content
       );
-      const affectedRows = await league.updateAdminRoleID(match.id, guildID);
-      console.log(match.id)
-      league.admin_role_id = match;
 
+      if (match !== 'fail') {
+        const affectedRows = await league.updateAdminRoleID(match.id, guildID);
+        console.log(match.id);
+        league.admin_role_id = match;
 
-      if (Number(affectedRows) > 0) {
-        message.channel.send(`League admin role set to: **${match.name}**`);
-        } else {
+        if (Number(affectedRows) > 0) {
+          message.channel.send(`League admin role set to: **${match.name}**`);
+          return 'success'
+        }
+      } else {
         message.channel.send(
-          "There was a problem updating the league admin role"
+          "There was a problem updating the league admin role, please use a role within in the server."
         );
       }
     } catch (e) {
       if (e instanceof abortSetup) {
-        return 'fail';
       }
-    return 'success'
     }
   },
 
   async eventNameSetup(message: Message, client: CommandClient) {
-    let league = await findGuildLeague(message.guild.id)
+    let league = await findGuildLeague(message.guild.id);
     message.channel.send("Enter the event text channels name");
     try {
       const eventChannelColl = await this.collectResponse(message);
@@ -108,7 +113,7 @@ export = {
   },
 
   async memberSetup(message: Message, client: CommandClient) {
-    let league = await findGuildLeague(message.guild.id)
+    let league = await findGuildLeague(message.guild.id);
     message.channel.send("Please enter the name of the member role");
     try {
       const memberRoleCollection = await this.collectResponse(message);
@@ -136,17 +141,24 @@ export = {
     }
   },
   async setupMaxteamSize(message: Message, client: CommandClient) {
-    let league = await findGuildLeague(message.guild.id)
-    message.channel.send('Please input the maximum team size. (For 5v5s the max teamsize would be 5)')
+    let league = await findGuildLeague(message.guild.id);
+    message.channel.send(
+      "Please input the maximum team size. (For 5v5s the max teamsize would be 5)"
+    );
     try {
       const teamsizeCollection = await this.collectResponse(message);
-      if(isNaN(Number(teamsizeCollection.content))){
-        message.channel.send('Please input a valid number!')
-        this.setupMaxteamSize(message, client)
+      if (isNaN(Number(teamsizeCollection.content))) {
+        message.channel.send("Please input a valid number!");
+        this.setupMaxteamSize(message, client);
       } else {
-        const affectedRows = await league.updateMaxTeamSize(Number(teamsizeCollection.content), message.guild.id)
+        const affectedRows = await league.updateMaxTeamSize(
+          Number(teamsizeCollection.content),
+          message.guild.id
+        );
         if (Number(affectedRows) > 0) {
-          message.channel.send(`Max teamsize set to: **${teamsizeCollection.content}**`);
+          message.channel.send(
+            `Max teamsize set to: **${teamsizeCollection.content}**`
+          );
         } else {
           message.channel.send(
             "There was a problem updating the max teamsize, please read the above message."
@@ -162,11 +174,15 @@ export = {
   },
 
   async collectResponse(message: Message) {
-   
-    const msg_filter = (m) => m.author.id === message.author.id
-    console.log(message.author.id)
+    const msg_filter = (m) => m.author.id === message.author.id;
+    console.log(message.author.id);
     const collectedMessage = await message.channel
-      .awaitMessages({filter: msg_filter, time: 2000000, max: 1, errors: ['time'] })
+      .awaitMessages({
+        filter: msg_filter,
+        time: 2000000,
+        max: 1,
+        errors: ["time"],
+      })
       .then((collection) => {
         return collection.first();
       })
@@ -178,4 +194,3 @@ export = {
     return collectedMessage;
   },
 };
-
